57acd82d4f5f2208de97db840dc971e93be88fc3
jakobiannn <jakobiannn@icloud.com>
2023-02-12 19:08:21 +0300

    init

---
+++ src/main/java/ru/jakobiannn/service/UserLoaderImpl.java
@@ -0,0 +1,13 @@
+package ru.jakobiannn.service;
+
+import org.springframework.stereotype.Component;
+import ru.jakobiannn.model.User;
+
+@Component
+public class UserLoaderImpl implements UserLoader {
+
+    public User getAllUsers() {
+
+        return null;
+    }
+}
